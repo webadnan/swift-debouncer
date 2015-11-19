57851a6c1f87bb7fe2f47311a8a04f42ddb077a8
SM Adnan <adnan@ailive.net>
2015-11-18 18:19:10 -0800

    First commit, the simple debouncer function for iOS swift

---
+++ Debouncer.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+class Debouncer: NSObject {
+    var callback: (() -> ())
+    var delay: Double
+    weak var timer: NSTimer?
+    
+    init(delay: Double, callback: (() -> ())) {
+        self.delay = delay
+        self.callback = callback
+    }
+    
+    func call() {
+        timer?.invalidate()
+        let nextTimer = NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: "fireNow", userInfo: nil, repeats: false)
+        timer = nextTimer
+    }
+    
+    func fireNow() {
+        self.callback()
+    }
+}
